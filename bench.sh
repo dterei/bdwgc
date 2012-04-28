20dc73d528fd879b35e53d4b052c97bf249904e2
David Terei <davidterei@gmail.com>
2012-04-27 19:49:28 -0700

    Add bench scripts.

---
+++ bench.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+FILE=normal-numbers.log
+
+git clone git://github.com/ivmai/libatomic_ops.git
+cd libatomic_ops
+./configure --disable-option-checking --disable-shared --disable-threads
+make
+
+cd ..
+autoreconf -vif
+automake --add-missing
+make clean
+make bench_clean
+./configure --disable-shared --disable-threads
+
+make
+make gctest
+make bench
+
+cd mxml
+make benchmxml
+cd ..
+
+clear
+echo "Done building, waiting 10 seconds for shit to cool down..."
+echo "Logging benchmark numbers to ${FILE}"
+sleep 10
+
+echo "Running gcbench"
+./gcbench > ${FILE}
+echo "\n==============================\n" >> ${FILE}
+
+sleep 2
+echo "Running gcbench 1"
+./gcbench 1 >> ${FILE}
+echo "\n==============================\n" >> ${FILE}
+
+sleep 2
+echo "Running sparse2"
+./sparse2 >> ${FILE}
+echo "\n==============================\n" >> ${FILE}
+
+sleep 2
+echo "Running mxml"
+./mxml/benchmxml LastName ./mxml/medline12n0668.xml >> ${FILE}
+
+echo "DONE! (check file ${FILE} for results)"
+
